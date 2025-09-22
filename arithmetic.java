aa18a55237a3fcd17f99792231cbca09ce328931
test-abc0909 <tabc73375@gmail.com>
2025-09-22 10:22:39 +0530

    Add files via upload

---
+++ arithmetic.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class arithmetic{
+    public static void main(String[] args){
+        int a=10;
+        int b=20;
+        int result=a*b;
+        System.out.println("result:"+result);
+    }
+}
