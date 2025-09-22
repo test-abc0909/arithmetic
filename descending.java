b6dff889f8c8a9af683cdc63567f5954df2ce5b6
test-abc0909 <tabc73375@gmail.com>
2025-09-22 10:58:49 +0530

    Add files via upload

---
+++ descending.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class descending {
+    public static void main(String[] args) {
+        int i = 10;
+        System.out.println("Numbers in descending Order (1 to 10):");
+        while (i >=1) {
+            System.out.println(i);
+            i--;
+        }
+    }
+}
